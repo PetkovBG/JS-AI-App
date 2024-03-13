b4b12846cd709425add2d2572ad4c990957fe711
PetkovBG <orlin.p.petkov@gmail.com>
2024-03-13 16:40:31 +0200

    add useApiRequests custom hook

---
+++ JS-AI-App/src/hooks/useApiRequests.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+
+const useApiRequests = (prompt) => {
+    const [error, setError] = useState(null);
+    const [promptData, setPromptData] = useState({});
+    const [locationData, setLocationData] = useState({});
+    const [weatherData, setWeatherData] = useState({});
+    const [weatherDescription, setWeatherDescription] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (!prompt) {
+                return;
+            }
+
+            try {
+                const promptDataRes = await PromptLocation(prompt);
+                setPromptData(promptDataRes);
+
+                const locationDataRes = await LocationCoordinates(promptDataRes.locationString);
+                setLocationData(locationDataRes);
+
+                const weatherDataRes = await WeatherData(locationDataRes);
+                setWeatherData(weatherDataRes);
+
+                const weatherDescriptionRes = await WeatherDescription(prompt, weatherDataRes)
+                setWeatherDescription(weatherDescriptionRes);
+            } catch (error) {
+                setError(error);
+                console.log('Error in API request: ', error);
+            }
+        };
+        fetchData();
+    }, [prompt]);
+
+    return { error, promptData, locationData, weatherData, weatherDescription }
+}
+
+export default useApiRequests;
